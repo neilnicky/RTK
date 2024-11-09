1628770b5c8fa82b7ef8e0128fd9c828bf3b10f5
neilnicky <neilnicholaskanat@gmail.com>
2024-11-09 13:21:56 +0530

    style refactor

--- counter-example-app/src/App.tsx
+++ counter-example-app/src/App.tsx
@@ -8,8 +8,8 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p className="Mid-text">
+          The Simple Counter App
         </p>
         <span>
           <span>Learn </span>
